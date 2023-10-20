79d98a7be8a4cc80a70722f8a4ec2e784652eac2
nicodev <ndevaux@gmail.com>
2023-10-20 11:56:19 +0200

    add sitemap

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,17 +3,23 @@ import Link from 'next/link'
 const Header = () => {
   return (
     <header className="sticky top-0 mx-auto border-b border-secondary bg-white py-5">
-      <nav className="container mx-auto flex items-center gap-5 text-lg md:text-2xl">
+      <nav className="container mx-auto flex flex-wrap items-center gap-5 text-lg md:text-2xl">
         <Link
           href="#avantages"
           className="transition-colors hover:text-lime-600"
         >
           Le cabinet
         </Link>
-        <Link href="#gallery" className="transition-colors hover:text-lime-600">
+        <Link
+          href="#gallery"
+          className="transition-colors hover:text-lime-600"
+        >
           Gallerie
         </Link>
-        <Link href="#tarifs" className="transition-colors hover:text-lime-600">
+        <Link
+          href="#tarifs"
+          className="transition-colors hover:text-lime-600"
+        >
           Tarifs
         </Link>
         <Link
