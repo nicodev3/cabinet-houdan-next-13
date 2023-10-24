12f4c0690c0b79f916961ac2d3e0ba66bd3deab2
nicodev <ndevaux@gmail.com>
2023-10-24 10:46:32 +0200

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,18 @@ import Footer from '@/app/components/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.location-cabinet-sceaux.fr/'),
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    title: 'Location espace conusltation Sceaux',
+    url: 'https://www.location-cabinet-sceaux.fr/',
+    siteName: 'Location espace conusltation Sceaux',
+    images: '/cabinet-houdan-paysage-1.jpg',
+    locale: 'fr_FR',
+    type: 'website',
+  },
   title: 'Location espace conusltation Sceaux',
   description:
     "Cabinet pluridisciplinaire à Sceaux offrant des bureaux équipés pour les professionnels. Emplacement central, idéal pour psychologues, thérapeutes et experts en bien-être. Réservez dès aujourd'hui.",
