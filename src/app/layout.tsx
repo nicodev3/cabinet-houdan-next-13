0111ee1b65299df498e07a30bd5808b540ec6a56
nicodev <ndevaux@gmail.com>
2024-01-03 15:43:51 +0100

    typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,14 +13,14 @@ export const metadata: Metadata = {
     canonical: '/',
   },
   openGraph: {
-    title: 'Location espace conusltation Sceaux',
+    title: 'Location espace consultation Sceaux',
     url: 'https://www.location-cabinet-sceaux.fr/',
-    siteName: 'Location espace conusltation Sceaux',
+    siteName: 'Location espace consultation Sceaux',
     images: '/cabinet-houdan-paysage-1.jpg',
     locale: 'fr_FR',
     type: 'website',
   },
-  title: 'Location espace conusltation Sceaux',
+  title: 'Location espace consultation Sceaux',
   description:
     "Cabinet pluridisciplinaire à Sceaux offrant des bureaux équipés pour les professionnels. Emplacement central, idéal pour psychologues, thérapeutes et experts en bien-être. Réservez dès aujourd'hui.",
 }
